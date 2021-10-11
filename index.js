945d1fd0138688a1e4981f6a9517fda474ef0da3
chrisdcalver <chrisdcalver@gmail.com>
2021-10-11 22:18:16 +0100

    localStorage Addition
    Trying to get localStorage to work within the extension.

--- index.js
+++ index.js
@@ -1,36 +1,36 @@
-/*
-1. When save button is pressed url from current tab is taken and saved to local storage
-    - Needs to grab the url from  the tab OR input field
-    - Assign the url to an array
-    - save the value to localStorage
-        - In order to save array to localStorage will need to convert to string
-2. Values from local storage is taken and displayed in an ul.
-    - parse local storage from string back into an array
-    - iterate through the array and display in html (+= to display each item)
-    - innerHTML need to include <li> and <a> tags
-3. When delete button is pressed localStorage, array and ul is cleared. 
-*/
-
+// Initialise variables
 let urlArr = []
 let inputEl = document.getElementById('input-el')
 let deleteBtn = document.getElementById('delete-btn')
 let ulEl = document.getElementById('ul-el')
+let leadsFromLocalStorage = JSON.parse(localStorage.getItem('storedURL'))
 
-document.getElementById('save-tab').addEventListener('click', function() {
-    urlArr.push(inputEl.value || window.location.href)
-    inputEl.value = ""
-    renderLeads()
+// If we exit browser although localStorage will have data urlARR var will clear so if localStorage is true the reassign its value to urlArr
+if (leadsFromLocalStorage) {
+    urlArr = leadsFromLocalStorage
+    renderLeads(urlArr)
 })
 
-function renderLeads() {
+// When saveTab Btn prssed we need to grab input.value OR url from browser and assign to array, then to localStorage and then clear input field
+document.getElementById('save-tab').addEventListener('click', function() {
+    urlArr.push(inputEl.value || window.location.href)
+    localStorage.setItem('storedURL', JSON.stringify(urlArr))
+    inputEl.value = ""
+    renderLeads(urlArr)
+})
+
+// Each time button is preseed it goes through the array then lists localStorage out in the DOM
+function renderLeads(leads) {
     let myList = ""
-    for (let i = 0; i < urlArr.length; i++) {
-        myList = urlArr[i]
-    }
-    ulEl.innerHTML += `<li>${myList}</li>`
+    for (let i = 0; i < leads.length; i++) {
+        myList = leads[i]
+    }  
+    ulEl.innerHTML += `<li><a href="${myList}">${myList}</a></li>`
 }
 
+// When the deleteBtn is pressed the array is cleared, localStorage is cleared and the DOM <li> is cleared.
 deleteBtn.addEventListener('click', function() {
     urlArr = []
+    localStorage.clear()
     ulEl.textContent = ""
 })
